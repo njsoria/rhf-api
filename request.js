87bc60355760e560fae2b51d6918681d8a1d755e
Nate Soria <nate@devkore.com>
2019-11-22 07:13:24 -0800

    prepped for aws lambda

--- request.js
+++ request.js
@@ -1,45 +1,53 @@
 var request = require("request-promise-native");
 
-let type = 'create';
+exports.handler = function(event, context, callback) {
 
-var getLoanData = {
-    token: null,
+    // grab the type and auth from the request
+    let type = event.queryStringParameters.type;
+    let auth = event.queryStringParameters.auth;
+    let loanID = event.queryStringParameters.loanID;
 
-    getData: function() {
-        return request.get({
-           "uri": "https://api.elliemae.com/encompass/v1/loans/6842aafb-fb9f-4384-b854-b4fbc24c16d6",
-           "json": true,
-           "headers": {
-                'Content-Type': 'application/json',
-                Authorization: 'Bearer y716IemHrbjI4PKq6hFfk5tjTKGn'
-            }
-        })
-        .then(function(response){
-            return JSON.stringify({type: type,
-                        data: response});
-        })
-        .catch(function(err){
-            if (err) throw new Error(err);
-        });
-    },
+    // create an object of functions for the main function
+    var getLoanData = {
+        token: null,
 
-    postData: function(data) {
-        return request.post({
-            "uri": "https://hooks.zapier.com/hooks/catch/5978432/o4jsk70/",
+        getData: function() {
+            return request.get({
+            "uri": "https://api.elliemae.com/encompass/v1/loans/" + loanID,
             "json": true,
             "headers": {
-                'Content-Type': 'application/json'
-            },
-            "body": data
-        });
+                    'Content-Type': 'application/json',
+                    Authorization: 'Bearer ' + auth
+                }
+            })
+            .then(function(response){
+                return JSON.stringify({type: type,
+                            data: response});
+            })
+            .catch(function(err){
+                if (err) throw new Error(err);
+            });
+        },
+
+        postData: function(data) {
+            return request.post({
+                "uri": "https://hooks.zapier.com/hooks/catch/5978432/o4jsk70/",
+                "json": true,
+                "headers": {
+                    'Content-Type': 'application/json'
+                },
+                "body": data
+            });
+        }
     }
-}
 
-function main() {
-    return getLoanData.getData()
-        .then(getLoanData.postData);
-}
+    function main() {
+        return getLoanData.getData()
+            .then(getLoanData.postData);
+    }
 
-main().then(function(result){
-    console.log(result);
-});
+    main().then(function(result){
+        console.log(result);
+        return result;
+    });
+}
